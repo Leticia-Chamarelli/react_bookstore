1e2b43dc7e8208bc7284a0b18e3f43d8b46b2737
Leticia <leticia.chamarelli@outlook.com>
2023-02-20 18:29:24 -0300

    al projetc commited

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
 import Header from './componentes/Header';
+import Pesquisa from './componentes/Pesquisa';
+import UltimosLancamentos from './componentes/UltimosLancamentos';
+import styled from 'styled-components';
+
+
+const AppContainer = styled.div` 
+    width: 100vw;
+    height: 100vh;
+    background-color: rgb(58, 68, 170);
+`
 
 function App() {
   return (
-    <div className='App'>
+    <AppContainer>
       <Header/>
-    </div>
+      <Pesquisa/>
+      <UltimosLancamentos/>
+    </AppContainer>
   );
 }
 
---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,9 @@
+import livro from '../../imagens/livro.png'
+import styled from 'styled-components'
+
+
+export const livros = [
+    {nome: 'Charm', id: 1, src:livro},
+    {nome: 'Cherish', id: 2, src:livro},
+    {nome: 'Holly', id: 3, src:livro},
+]
